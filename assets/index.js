d350893c3d33ac50b2ceaf0f6b560ae3a337b3ca
Carlos Quadros <carlosalbertoquadros@gmail.com>
2021-01-08 20:50:00 -0300

    safe commit

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,65 @@
+function invalid(selector) {
+    selector.addClass("is-invalid")
+}
+function valid(selector) {
+    selector.addClass("is-valid")
+}
+
+//========== CPF validation =============//
+
+
+$(function () {
+    $('#inputCpf').blur(function () {
+        var cpf = $('#inputCpf').val().replace(/[^0-9]/g, '');
+
+        if (cpf.length == 11) {
+            if (cpf == "11111111111" ||
+                cpf == "22222222222" ||
+                cpf == "33333333333" ||
+                cpf == "44444444444" ||
+                cpf == "55555555555" ||
+                cpf == "66666666666" ||
+                cpf == "77777777777" ||
+                cpf == "88888888888" ||
+                cpf == "99999999999") {
+                invalid($('#inputCpf'))
+                $('#inputCpf').val('');
+            } else {
+                $('#inputCpf').removeClass("is-invalid")
+                valid($('#inputCpf'))
+
+
+            }
+            var v = [];
+
+            //Calcula o primeiro dígito de verificação.
+            v[0] = 1 * cpf[0] + 2 * cpf[1] + 3 * cpf[2];
+            v[0] += 4 * cpf[3] + 5 * cpf[4] + 6 * cpf[5];
+            v[0] += 7 * cpf[6] + 8 * cpf[7] + 9 * cpf[8];
+            v[0] = v[0] % 11;
+            v[0] = v[0] % 10;
+
+            //Calcula o segundo dígito de verificação.
+            v[1] = 1 * cpf[1] + 2 * cpf[2] + 3 * cpf[3];
+            v[1] += 4 * cpf[4] + 5 * cpf[5] + 6 * cpf[6];
+            v[1] += 7 * cpf[7] + 8 * cpf[8] + 9 * v[0];
+            v[1] = v[1] % 11;
+            v[1] = v[1] % 10;
+
+            //Retorna Verdadeiro se os dígitos de verificação são os esperados.
+            if ((v[0] != cpf[9]) || (v[1] != cpf[10])) {
+                
+                invalid($('#inputCpf'))
+                $('#inputCpf').val('');
+            }
+        }
+        else {
+          
+            $('#inputCpf').val('');
+        }
+    });
+});
+
 //========== api cep consult: (https://postmon.com.br/) implement ======//
 
 var zipCode = $("#inputPostCode");
@@ -20,7 +82,11 @@ zipCode.blur((Event) => {
     fetch(`https://api.postmon.com.br/v1/cep/${fomatedZip}`, options)
         .then(response => {
             response.json()
-                .then(data => showData(data))
+                .then(data => showData(data),
+                $('#inputPostCode').removeClass("is-invalid"),
+                valid($('#inputPostCode')))
         })
-        .catch(e => alert("Insira um cep válido"))
+        .catch(e => $('#inputPostCode').val(''),invalid($("#inputPostCode")),
+        )
+            
 })
