72e1b2d78a53229cfe5eb9676ee5e9f4d835199a
Carlos Quadros <carlosalbertoquadros@gmail.com>
2021-01-08 02:37:25 -0300

    zip code validation done

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,26 @@
-var zipCode = document.querySelector("#inputPostCode").value
+//========== api cep consult: (https://postmon.com.br/) implement ======//
 
-function FindZipCode(){
-    console.log(zipCode);
+var zipCode = $("#inputPostCode");
+const showData = (result) => {
+    for (const field in result) {
+        if (document.querySelector("#input_" + field)) { //code didn't work with jquery shorthands, 
+            document.querySelector("#input_" + field).value = result[field]
+        }
+    }
 }
+zipCode.blur((Event) => {
+    let fomatedZip = zipCode.val().replace("-", "")
+
+    const options = {
+        method: "GET",
+        mode: "cors",
+        cache: "default"
+    }
+
+    fetch(`https://api.postmon.com.br/v1/cep/${fomatedZip}`, options)
+        .then(response => {
+            response.json()
+                .then(data => showData(data))
+        })
+        .catch(e => alert("Insira um cep válido"))
+})
